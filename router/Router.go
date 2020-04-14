7ac11ea1efeba8abd5b5d848d520e2f786f9a7a7
Jaedsada Chaisorn <jaedsada@Jaedsadas-MacBook-Pro.local>
2020-04-14 22:23:37 +0700

    first commit

--- router/Router.go
+++ router/Router.go
@@ -1,9 +1,10 @@
 package router
 import (
-	"project-demo/controller/home"
-	"project-demo/controller/home2"
+	"project-demo/controller/controllerA"
+	"project-demo/controller/controllerB"
 	"github.com/gin-gonic/gin"
 )
+
 func RouterInit() *gin.Engine {
 	r := gin.Default()
 	r.GET("/packageA", packageA.Index1)
